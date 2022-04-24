1e9c015878abe197a7f3146c9effdcbd453e9843
Narmin <mnermin064@gmail.com>
2022-04-24 14:09:24 +0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+
+// Fourth way
+let theme = localStorage.getItem('data-theme');
+const Btn = document.querySelector('.btn1');
+const logo = document.querySelector('.img');
+
+const changeThemeToBlue = () =>{
+    document.documentElement.setAttribute("data-theme", "blue");
+    localStorage.setItem("data-theme", "blue");
+    if(logo.src.match('assets/img/yasil.png')){
+        logo.src = 'assets/img/goy.png' ;
+    }
+    else{
+        logo.src = 'assets/img/yasil.png' ;
+    }
+}
+
+const changeThemeToGreen = () =>{
+    document.documentElement.setAttribute("data-theme", "green");
+    localStorage.setItem("data-theme", 'green');
+    if(logo.src.match('assets/img/yasil.png')){
+        logo.src = 'assets/img/goy.png' ;
+    }
+    else{
+        logo.src = 'assets/img/yasil.png' ;
+    }
+}
+
+if(theme === 'blue'){
+    changeThemeToBlue()
+}
+
+Btn.addEventListener('click', ()=> {
+    let theme = localStorage.getItem('data-theme');
+    if (theme ==='blue'){
+        changeThemeToGreen()
+    }else{
+        changeThemeToBlue()
+    }
+   
+});
+
+
+// const Btn2 = document.querySelector('.btn2');
+// // const logo = document.querySelector('.img');
+// const myFunc = () =>{
+//     if(logo.src.match('assets/img/yasil.png')){
+//         logo.src = 'assets/img/goy.png' ;
+//     }
+//     else{
+//         logo.src = 'assets/img/yasil.png' ;
+//     }
+// }
+
+// Btn2.addEventListener('click',myFunc);
