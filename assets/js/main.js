75030051af953d418ee01c3579f0dec0930fe6df
Narmin <mnermin064@gmail.com>
2022-04-30 08:17:00 +0400

    responsive menu added with jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,39 @@
 // #####################################
-// Adding local storage 
+// Adding local storage with Vanillajs
 // #####################################
 let theme = localStorage.getItem('data-theme');
 const Btn = document.querySelector('.btn1');
 const logo = document.querySelectorAll('.img');
 
-const changeThemeToBlue = () =>{
-    document.documentElement.setAttribute("data-theme", "blue");
-    localStorage.setItem("data-theme", "blue");
-    for(var x= 0;x<logo.length;x++){
-        if(logo[x].src.match('assets/img/green.png')){
-            logo[x].src = 'assets/img/blue.png' ;
-        }
-        else{
-            logo[x].src = 'assets/img/green.png' ;
-        }
+const changeThemeToBlue = () => {
+  document.documentElement.setAttribute("data-theme", "blue");
+  localStorage.setItem("data-theme", "blue");
+  for (var x = 0; x < logo.length; x++) {
+    if (logo[x].src.match('assets/img/green.png')) {
+      logo[x].src = 'assets/img/blue.png';
     }
+    else {
+      logo[x].src = 'assets/img/green.png';
+    }
+  }
 }
 
 
-const changeThemeToGreen = () =>{
-    document.documentElement.setAttribute("data-theme", "green");
-    localStorage.setItem("data-theme", 'green');
-    for(var x= 0;x<logo.length;x++){
-        if(logo[x].src.match('assets/img/green.png')){
-            logo[x].src = 'assets/img/blue.png' ;
-        }
-        else{
-            logo[x].src = 'assets/img/green.png' ;
-        }
+const changeThemeToGreen = () => {
+  document.documentElement.setAttribute("data-theme", "green");
+  localStorage.setItem("data-theme", 'green');
+  for (var x = 0; x < logo.length; x++) {
+    if (logo[x].src.match('assets/img/green.png')) {
+      logo[x].src = 'assets/img/blue.png';
     }
+    else {
+      logo[x].src = 'assets/img/green.png';
+    }
+  }
 }
 
-if(theme === 'blue'){
-    changeThemeToBlue()
+if (theme === 'blue') {
+  changeThemeToBlue()
 }
 
 Btn.addEventListener('click', ()=> {
@@ -43,44 +43,77 @@ Btn.addEventListener('click', ()=> {
     }else{
         changeThemeToBlue()
     }
-   
+
 });
+
+
+// #####################################
+// Adding local storage with Jquuery
+// #####################################
+// $(document).ready(function() {
+//   $('.btn1').click(function (){
+//     let theme = localStorage.getItem('data-theme');
+//     if (theme === 'blue') {
+//       changeThemeToGreen()
+//     } else {
+//       changeThemeToBlue()
+//     }
+//   })
+// })
+
+
+// #####################################
+// Left-menu
+// #####################################
+
+
+  $('.left-menu').click(function(){
+    $('.right-menu'). removeClass('come');
+  });
+
+
+  $('.close').click(function(){
+    $('.right-menu'). addClass('come');
+  });
+
+
+
 
 // #####################################
 // Slider
 // #####################################
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    speed: 700,
-    effect: 'fade',
-    fadeEffect: {
-        crossFade: true
-      },
-      autoplay: {
-        delay: 3000,
-      },
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    
-  });
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  speed: 700,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  autoplay: {
+    delay: 3000,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+});
 
 // #####################################
 // Loading page
 // #####################################
 
 const loader = document.querySelector(".load");
-window.onload = function(){
-  setTimeout(function(){
+window.onload = function () {
+  setTimeout(function () {
     loader.style.opacity = "0";
     loader.style.transition = ".4s";
-    setTimeout(function(){
+    setTimeout(function () {
       loader.style.display = "none";
     }, 2000);
-  },2200);
+  }, 2200);
 }
